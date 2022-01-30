4291c5e25973a4d6f44ba39eb5798513ec8dc521
Daniel Bailo <danbailoufms@gmail.com>
2022-01-30 20:42:10 -0300

    feat: sending email reminder if has a new payment

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from models.reminder import Reminder
+from models.email import Email
 
 if __name__ == '__main__':
-    reminder = Reminder()
+    reminder = Reminder(student_id=None)
+    email = Email()
+    if reminder.has_new_payment():
+        email.send_message()
