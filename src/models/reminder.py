f1a45676227045e8fc1cff76e97a0ac062049d14
Daniel Bailo <danbailoufms@gmail.com>
2022-01-30 20:41:26 -0300

    hotfix: getting info from defaults and passing id from validator

--- src/models/reminder.py
+++ src/models/reminder.py
@@ -1,18 +1,27 @@
-from serializers import student_serializer
-import requests
 import datetime
+import requests
+from typing import Optional
+
+from defaults import STUDENT_ID
+from serializers.student_serializer import StudentSerializer
 
 
 class Reminder:
     URL: str = 'https://www.fnde.gov.br/digef/rs/spba/publica/pagamento/'
-    __student: str = None
+    __student_id: str = None
+
+    def __init__(self, student_id: Optional[str] = None) -> None:
+        self.__student_id = student_id
 
     @property
-    def student(self):
-        return student_serializer.STUDENT_ID
+    def student_id(self):
+        if self.__student_id is None:
+            return STUDENT_ID
+        return self.__student_id
 
     def _get_last_payment(self) -> datetime.date:
-        response = requests.get(self.URL + self.student)
+        student_id = StudentSerializer(id=self.student_id)
+        response = requests.get(self.URL + student_id.id)
         last_payment = (response.json()["programas"][0]['entidades']
                         ['15461510000133']['funcoes']['49']['pagamentos'][-1])
         date_payment = last_payment['data'].split('/')
--- src/serializers/student_serializer.py
+++ src/serializers/student_serializer.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
-from defaults import STUDENT_ID
 
 
 class StudentSerializer(BaseModel):
-    student_id: str = STUDENT_ID
+    id: str
