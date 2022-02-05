d0ee1c3bd7d63723b882a1068fa123afcfe58137
Daniel Bailo <danbailoufms@gmail.com>
2022-02-05 18:35:11 -0300

    feat[utils]: utils file to provide public utils functions

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,8 @@ import json
 def load_json(path: str) -> dict:
     with open(path, 'r') as file:
         return json.load(file)
+
+
+def get_recipients_from_txt():
+    with open('recipients_emails.txt', 'r') as file:
+        return file.readlines()
