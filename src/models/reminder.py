65603e68267428c36dbf689105d6c3059260651b
Daniel Bailo <danbailoufms@gmail.com>
2022-02-05 14:49:55 -0300

    refactor: create method to request data from api

--- src/models/reminder.py
+++ src/models/reminder.py
@@ -2,6 +2,8 @@ import datetime
 import requests
 from typing import Optional
 
+from pydantic import Json
+
 from defaults import STUDENT_ID
 from serializers.student_serializer import StudentSerializer
 
@@ -19,11 +21,15 @@ class Reminder:
             return STUDENT_ID
         return self.__student_id
 
-    def _get_last_payment(self) -> datetime.date:
+    def get_student_response(self) -> Json:
         student_id = StudentSerializer(id=self.student_id)
         response = requests.get(self.URL + student_id.id)
-        last_payment = (response.json()["programas"][0]['entidades']
-                        ['15461510000133']['funcoes']['49']['pagamentos'][-1])
+        return response.json()
+
+    def _get_last_payment(self) -> datetime.date:
+        last_payment = (self.get_student_response()["programas"][0]
+                        ['entidades']['15461510000133']['funcoes']
+                        ['49']['pagamentos'][-1])
         date_payment = last_payment['data'].split('/')
         return datetime.date(
             year=int(date_payment[2]),
