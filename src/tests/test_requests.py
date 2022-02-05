b0bb0ac77e69261fdb34e038ca4f6f664cef6e93
Daniel Bailo <danbailoufms@gmail.com>
2022-02-05 14:48:37 -0300

    tests: added expected keys in json responde

---
+++ src/tests/test_requests.py
@@ -0,0 +1,22 @@
+import unittest
+import requests
+import json
+
+import responses
+from responses import matchers
+
+from models.reminder import Reminder
+from utils import load_json
+
+
+class TestCase(unittest.TestCase):
+
+    def test_response(self):
+        body = load_json('tests/fixtures/jsons/response.json')
+        student = Reminder().get_student_response()
+        for k in body.keys():
+            self.assertIn(k, student.keys())
+
+
+if __name__ == '__main__':
+    unittest.main()
