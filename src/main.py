3b50b1efa2b4ed7c64862295510bc6aafb55f23d
Daniel Bailo <danbailoufms@gmail.com>
2022-02-09 23:06:43 -0300

    hotfix[dev]: testing resources

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
 from models.reminder import Reminder
 from models.email import Email
+from models.database import Database
+
 
 if __name__ == '__main__':
-    reminder = Reminder(student_id=None)
-    email = Email()
-    if reminder.has_new_payment():
-        email.send_message()
+    database = Database()
+    reminder = Reminder()
+    # email = Email()
+
+    has_paid = reminder.has_new_payment()
+    # if not has_paid:
+    #     exit()
+    data = {
+        'id': None,
+        'last_payment': None,
+        'has_sent_email': None,
+    }
+    Payment = database.TABLES_HOOK.get('PAYMENT')
+    session = database.connect_db()
+    breakpoint()
+    if last_payment := session.query(Payment).order_by(
+                               Payment.id.desc()).first():
+        if not last_payment.has_sent_email:
+            payment = Payment(has_sent_email=True)
+            session = database.connect_db()
+            session.add(payment)
+            session.close()
